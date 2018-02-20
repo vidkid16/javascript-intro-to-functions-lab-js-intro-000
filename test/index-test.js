a1236bd7495f784115a2c33523290f054c80d342
Eliot Garcia <eag0828@gmail.com>
2018-02-20 16:18:48 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -12,7 +12,9 @@ describe('whisper(string)', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
 })
-
+function whisper(string) {
+  return string.toLowerCase()
+}
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
