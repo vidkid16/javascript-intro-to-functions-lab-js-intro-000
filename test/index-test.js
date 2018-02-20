e35ffbd2874d68c43c42b50fc4d87931bc27a005
Eliot Garcia <eag0828@gmail.com>
2018-02-20 16:50:44 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -60,12 +60,4 @@ describe('sayHiToGrandma(string)', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED" 
-  } else if (string === "I love you, Grandma") {
-    return "I love you, too."
-  }
-  }
+
