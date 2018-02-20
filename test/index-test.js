262754010fd3b6d5223114ab2c9c44a11a3fff84
Eliot Garcia <eag0828@gmail.com>
2018-02-20 16:28:58 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -54,8 +54,7 @@ describe('sayHiToGrandma(string)', function() {
 function sayHiToGrandma(string) {
   if (sayHiToGrandma.toLowerCase() === lowercase) {
     return "I can't hear you!"
-  } else {
-    return
+  }
   }
 }
   it('returns "YES INDEED!" if `string` is uppercase', function() {
